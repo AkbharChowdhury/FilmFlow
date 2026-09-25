c17ebcafb6db871fffa21a4928921fbce4139922
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2026-09-25 09:35:04 +0100

    a

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Movie:
         for movie in movies:
             genre_string: str = movie["genres"]
             genres: list[str] = [genre.strip() for genre in genre_string.split("|")]
-            sorted_genres: list[str] = sorted(genres, key=str.lower)
+            sorted_genres: list[str] = sorted(genres, key=str.casefold)
             movie["genres"] = " | ".join(sorted_genres)
         sorted_movies = sorted(movies, key=lambda m: m["title"].casefold())
         return sorted_movies
